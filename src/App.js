5123ed47e4e1ffe5813e1261bc9d68460dae84c7
Himanshu <xylogramori@gmail.com>
2023-10-02 14:02:15 +0530

    QrPage setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom';
 import Login from './pages/Login';
+import QrPage from './pages/QrPage';
 const App = () => {
   return (
     <Routes>
       <Route exact path='/' element={<Login/>}/>
-      
-
+      <Route exact path='/qr' element={<QrPage/>}/>
     </Routes>
   )
 }
---
+++ src/pages/QrPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QrPage = () => {
+  return (
+    <div>
+      Qr
+    </div>
+  )
+}
+
+export default QrPage
